0f4b7d3a7fbaf4dfdd7393678962ceb03b27add4
Jair20024 <jairestefano1234@gmail.com>
2025-05-27 20:47:03 -0500

    Patron: "Circuit Breaker" en Pagos al llamar a Pedidos

--- pagos-service/src/main/java/com/coffeeandcode/pagos_service/service/PagoService.java
+++ pagos-service/src/main/java/com/coffeeandcode/pagos_service/service/PagoService.java
@@ -1,26 +1,32 @@
 package com.coffeeandcode.pagos_service.service;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class PagoService {
+    private static final Logger logger = LoggerFactory.getLogger(PagoService.class);
     private final String PEDIDOS_URL = "http://localhost:8082/pedidos/";
 
     @Autowired
     private WebClient webClient;
 
+    @CircuitBreaker(name = "pagosServiceCB", fallbackMethod = "fallbackMarcarPedidoComoPagado")
     public boolean marcarPedidoComoPagado(Long pedidoId) {
-        try {
-            webClient.put()
-                    .uri(PEDIDOS_URL + pedidoId + "/pagar")
-                    .retrieve()
-                    .bodyToMono(Void.class)
-                    .block();
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
+        webClient.put()
+                .uri(PEDIDOS_URL + pedidoId + "/pagar")
+                .retrieve()
+                .bodyToMono(Void.class)
+                .block();
+        return true;
+    }
+
+    public boolean fallbackMarcarPedidoComoPagado(Long pedidoId, Throwable t) {
+        logger.error("Error al marcar pedido como pagado (fallback): {}", t.getMessage());
+        return false;
     }
 }
