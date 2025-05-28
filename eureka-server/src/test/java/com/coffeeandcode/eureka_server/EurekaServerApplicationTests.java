12a1275af2c6911b4af9e01d62294e991074a864
Jair20024 <jairestefano1234@gmail.com>
2025-05-27 22:14:03 -0500

    Patron: "Api Gateway" con eureka

---
+++ eureka-server/src/test/java/com/coffeeandcode/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffeeandcode.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
